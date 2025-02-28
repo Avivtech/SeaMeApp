efa9197fe2b76f2ca8a5d843c351b1284f56393b
Avivtech <avivtech@gmail.com>
2025-02-28 05:56:06 +0200

    a

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from "./App.js";
 
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error('Root element not found');
+document.addEventListener("DOMContentLoaded", () => {
+	const rootElement = document.getElementById("root");
+	if (!rootElement) throw new Error("Root element not found");
 
-createRoot(rootElement).render(React.createElement(App));
+	createRoot(rootElement).render(React.createElement(App));
+});
