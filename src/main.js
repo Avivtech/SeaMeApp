3d9eb1426a950d1d5e07f5a231d235aa2b414fa3
Avivtech <avivtech@gmail.com>
2025-02-28 04:33:31 +0200

    a

--- src/main.js
+++ src/main.js
@@ -6,4 +6,6 @@ import './index.css'
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error('Root element not found');
 
+console.log("React version:", React.version);
+
 createRoot(rootElement).render(<App />);
