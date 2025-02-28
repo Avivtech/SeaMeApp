47c72b5785d4c397bcd0102df6d1ea636312ebae
Avivtech <avivtech@gmail.com>
2025-02-28 06:03:51 +0200

    a

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from "./App.js";
+import App from "./App.jsx";
 
 document.addEventListener("DOMContentLoaded", () => {
 	const rootElement = document.getElementById("root");
